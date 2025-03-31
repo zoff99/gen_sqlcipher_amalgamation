6178ecadc52bff97166630dd787db1958cbc09a0
zoff99 <zoff99@users.noreply.github.com>
2025-03-31 19:35:41 +0200

    tweaks for version 4.7.0

--- runme.sh
+++ runme.sh
@@ -10,7 +10,7 @@ rm -Rf sqlcipher/
 git clone https://github.com/sqlcipher/sqlcipher
 cd sqlcipher/
 git checkout "$_SQLCIPHER_"
-./configure  --disable-tcl --disable-shared --enable-static=yes --enable-tempstore=yes CFLAGS="-DSQLITE_HAS_CODEC -DSQLCIPHER_CRYPTO_OPENSSL"
+./configure  --disable-tcl --disable-shared --with-tempstore=yes CFLAGS="-DSQLITE_HAS_CODEC -DSQLCIPHER_CRYPTO_OPENSSL -DSQLITE_EXTRA_INIT=sqlcipher_extra_init -DSQLITE_EXTRA_SHUTDOWN=sqlcipher_extra_shutdown"
 make sqlite3.c
 
 ls -hal sqlite3.h sqlite3ext.h sqlite3.c
